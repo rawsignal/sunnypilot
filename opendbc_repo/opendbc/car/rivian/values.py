c41a62d79b80e9db0ecfaa932f01529bb453dec7
Uncle-Tony <211085818+Uncle-Tony@users.noreply.github.com>
2026-03-07 18:37:38 -0600

    Rivian: low speed torque 290

--- opendbc_repo/opendbc/car/rivian/values.py
+++ opendbc_repo/opendbc/car/rivian/values.py
@@ -116,8 +116,8 @@ class CarControllerParams:
   # 250 is ~2.8 m/s^2 above 17 m/s, then linearly ramps to ~1.6 m/s^2 from 17 m/s to 9 m/s
   # TODO: it is theorized older models have different steering racks and achieve down to half the
   #  lateral acceleration referenced here at all speeds. detect this and ship a torque increase for those models
-  STEER_MAX = 250  # 300 low speed, 250 at highway
-  STEER_MAX_LOOKUP = [9, 17], [300, 250]
+  STEER_MAX = 250  # 290 low speed, 250 at highway
+  STEER_MAX_LOOKUP = [9, 17], [290, 250]
   STEER_STEP = 1
   STEER_DELTA_UP = 3  # torque increase per refresh
   STEER_DELTA_DOWN = 5  # torque decrease per refresh
